de593f3b1e203cc1bef322ad70e6b11f02ee196f
Shanna Ginos <Shanna@Dustins-MBP.directcom.com>
2021-03-23 15:55:23 -0600

    added completed lab to git repo

---
+++ linked list stuff/src/linked-list.js
@@ -0,0 +1,83 @@
+class LinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this._size = 0;
+  }
+
+  insert(data) {
+    const newNode = new LinkedListNode(data);
+    // check the head and see if it's a node or not
+    if (!this.head) {
+      this.head = newNode;
+      this._size += 1;
+    } else {
+      // loop through the nodes starting at the head until you find
+      // the one pointing to null
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      // insert the node there
+      current.next = newNode;
+      this._size += 1;
+    }
+  }
+
+  contains(data) {
+    // loop through the nodes starting at the head and if there is one with the data
+    // return true otherwise check it's next
+    let current = this.head;
+    while (current) {
+      if (current.data == data) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    // if we never found the data return false
+    return false;
+  }
+
+  remove(data) {
+    // loop through the nodes starting at the head and if there is one with the data
+    // then set the previous node's .next to the following node
+    if (this.head.data === data) {
+      this.head = this.head.next;
+      this._size--;
+    } else {
+      let previous = this.head;
+      let current = previous.next;
+      while (current) {
+        if (current.data === data) {
+          previous.next = current.next;
+          current = current.next;
+          this._size--;
+        } else {
+          previous = current;
+          current = current.next;
+        }
+      }
+    } 
+  }
+
+  size() {
+    return this._size;
+  }
+}
+
+const list = new LinkedList();
+console.log(JSON.stringify(list));
+list.insert(12);
+list.insert(99);
+list.insert(37);
+list.insert(56);
+list.remove(99);
+console.log(list.contains(99));
+
